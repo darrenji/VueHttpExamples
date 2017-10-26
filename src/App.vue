5d2be956534953e630da786da878994400b11ec6
darren <764190362@qq.com>
2017-10-26 08:31:18 +0800

    post数据成功

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-3 col-md-offset-3">
                 <h3>Https</h3>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input type="text" class="form-control" v-model="user.email">
+                </div> 
+                <button class="btn btn-primary" @click="submit">Submit</button>           
             </div>
         </div>
     </div>
@@ -11,7 +20,26 @@
 
 <script>
     export default {
-
+        data(){
+            return {
+              user: {
+                  username: '',
+                  email: ''
+              }  
+            };
+        },
+        methods: {
+            submit(){
+                //console.log(this.user);
+                
+                this.$http.post('https://vuejs-http-a3dd3.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                });
+            }
+        }
     }
 </script>
 
