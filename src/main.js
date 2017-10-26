92bccbe4ea7ffa890dafdb499477a8d06c44ae1b
darren <764190362@qq.com>
2017-10-26 08:57:49 +0800

    请求拦截，Vue.http.interceptors.push

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ Vue.use(VueResource);
 
 //vue-resource的全局配置
 Vue.http.options.root = 'https://vuejs-http-a3dd3.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+    if(request.method == 'POST'){
+        request.method = 'PUT';
+    }
+    next();
+});
 
 new Vue({
   el: '#app',
