fed9318f91470f7a78be80b61cc01cd780c20ea7
darren <764190362@qq.com>
2017-10-26 09:33:14 +0800

    动态设置uri

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
                 </div> 
                 <button class="btn btn-primary" @click="submit">Submit</button>   
                 <hr>
+                <input type="text" class="form-control" v-model="node">
+                <br><br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>  
                 <br/><br/>
                 <ul class="list-group">
@@ -33,7 +35,8 @@
                   email: ''
               },
               users: [],
-              resource: {}
+              resource: {},
+              node: 'data'
             };
         },
         methods: {
@@ -52,7 +55,19 @@
                 
             },
             fetchData(){
-                this.$http.get('data.json')
+//                this.$http.get('data.json')
+//                    .then(response => {
+//                        return response.json();
+//                    })
+//                    .then(data => {
+//                        const resultArray = [];
+//                        for(let key in data){
+//                            resultArray.push(data[key]);
+//                        }
+//                        this.users = resultArray;
+//                    });
+                
+                this.resource.getData({node:this.node})
                     .then(response => {
                         return response.json();
                     })
@@ -67,9 +82,10 @@
         },
         created(){
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET' }
             };
-            this.resource = this.$resource('data.json',{},customActions);
+            this.resource = this.$resource('{node}.json',{},customActions);
         }
     }
 </script>
